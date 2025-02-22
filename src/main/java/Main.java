0b8892809a4c51ab59bfd35a6c281a8deba016f8
Collin Morrison <morricollin@gmail.com>
2025-02-21 20:53:14 -0700

    should be working now?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,27 +11,32 @@ public class Main {
 
         String operation = input[0];
 
-        switch (operation) {
-            case "add":
-                System.out.println(calc.add(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
-                break;
-            case "subtract":
-                System.out.println(calc.subtract(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
-                break;
-            case "multiply":
-                System.out.println(calc.multiply(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
-                break;
-            case "binary":
-                System.out.println(calc.intToBinaryNumber(Integer.parseInt(input[1])));
-                break;
-            case "divide":
-                System.out.println(calc.divide(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
-                break;
-            case "fibonacci":
-                System.out.println(calc.fibonacciNumberFinder(Integer.parseInt(input[1])));
-                break;
-            default:
-                System.out.println("Unknown command.");
+        Boolean keepRunning = true;
+        while (keepRunning) {
+            switch (operation) {
+                case "add":
+                    System.out.println(calc.add(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+                    break;
+                case "subtract":
+                    System.out.println(calc.subtract(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+                    break;
+                case "multiply":
+                    System.out.println(calc.multiply(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+                    break;
+                case "binary":
+                    System.out.println(calc.intToBinaryNumber(Integer.parseInt(input[1])));
+                    break;
+                case "divide":
+                    System.out.println(calc.divide(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+                    break;
+                case "fibonacci":
+                    System.out.println(calc.fibonacciNumberFinder(Integer.parseInt(input[1])));
+                    break;
+                case "quit":
+                    keepRunning = false;
+                default:
+                    System.out.println("Unknown command.");
+            }
         }
         
 
